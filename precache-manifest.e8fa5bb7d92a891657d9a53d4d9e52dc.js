ef8552831e6f77fcadf8cd14310a980f3526e0ac
Lora Levitska <larakad94@gmail.com>
2021-01-20 17:46:29 +0200

    Updates

---
+++ precache-manifest.e8fa5bb7d92a891657d9a53d4d9e52dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321ec515436558079916223cc232669c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9389eae8b9788fa89be1",
+    "url": "./static/css/main.b3832233.chunk.css"
+  },
+  {
+    "revision": "4b964fc25ecc0b658447",
+    "url": "./static/js/2.18166693.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18166693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9389eae8b9788fa89be1",
+    "url": "./static/js/main.70f997ae.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
